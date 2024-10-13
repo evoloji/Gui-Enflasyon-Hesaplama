95e12f0d16777a549fdfcfb5671c550fa74a71ef
evoloji <evoloji@192.168.1.14>
2024-10-14 00:51:12 +0300

    clean code

--- ehesap.py
+++ ehesap.py
@@ -12,31 +12,35 @@ alanlar = ("Poliçe Başlangıç Tarihi", "Hasar Tarihi", "Enflasyon Oranı", "S
 
 #Hesaplama ve girdi tanımları
 def enflasyon_hesap(entries):
+    if not entries["Enflasyon Oranı"].get() or not entries["Sigortalı Bedeli"].get():
+     print("Lütfen tüm alanları doldurunuz.")
+     return
+
+
     #Tarihli entryleri tanımlama
     police_ilk = str(entries["Poliçe Başlangıç Tarihi"].get())
     hasar_tar =  str(entries["Hasar Tarihi"].get())
     pit = datetime.datetime.strptime(police_ilk, "%d.%m.%Y")
     htt = datetime.datetime.strptime(hasar_tar, "%d.%m.%Y")
 
-    #Enflasyon oranı girdisi
-    enf_oran = (float(entries["Enflasyon Oranı"].get()) / 100)
-    sig_bed = float(entries["Sigortalı Bedeli"].get())  
-    
+    try:
+     enf_oran = (float(entries["Enflasyon Oranı"].get()) / 100)
+     sig_bed = float(entries["Sigortalı Bedeli"].get())  
+    except ValueError:
+     print("Tarih formatı hatalı, lütfen gg.aa.YYYY formatında girin.")
+     return
+
     #Hasarlı Gün Sayısını Hesaplama (Poliçe Başlangıç Tarihi - Hasar Tarihi )
     gunsayi = (htt - pit).days
     global sayi
     sayi = gunsayi
-    #print(gunsayi)
-    
+
     #Vade Hesaplama
     vade=float(365)
-    #print(vade)
-    #vade =  pst - pit
-    #Enflasyon Katsayı Hesaplama ""Gün/Vade x (Enf Oranı %) +1""
     enflasyon_artis_katsayi = ((float(gunsayi) /vade) * enf_oran) + 1
-    #Sigorta Bedeli * Katsayı
     enf_sig_bed = enflasyon_artis_katsayi *sig_bed
-    
+
+    #Sigorta Bedeli * Katsayı
     enflasyon_artis_katsayi = ("%0.7f" % enflasyon_artis_katsayi).strip()
 
     #locale ayarkarı
@@ -51,14 +55,16 @@ def enflasyon_hesap(entries):
     
     entries["Enflasyonlu Sigorta Bedeli"].delete(0,END)
     entries["Enflasyonlu Sigorta Bedeli"].insert(0,f)
-     
+
+    if 'left' in globals():
+     left.destroy()  # Mevcut label'ı temizle
+    
     labelframe = LabelFrame(root,text = "Hasarlı Gün Sayısı")
     labelframe.pack(fill = "both", expand = "yes")
     
     left = Label(labelframe, text = sayi)
     left.pack() 
 
-    
 #Şablon oluşturması    
 def makeform(root, alanlar):
    entries = {}
@@ -76,19 +82,10 @@ def makeform(root, alanlar):
 if __name__ == '__main__':
    root = Tk()
    ents = makeform(root, alanlar)
-   #root.bind('<Return>', (lambda event, e=ents: fetch(e))) 
-   def fetch(entries): 
-    for entry in entries: 
-        field = entry[0]
-        text = entry[1].get() 
-        print ("%s: %s" %(field,text)) 
    root.title("Enflasyon Hesap")
+
    b1 = Button(root, text='Enflasyon Hesapla', 
                command=(lambda e=ents: enflasyon_hesap(e)))
    b1.pack(side=LEFT, padx=5, pady=5)
+
    root.mainloop()
-   b1 = Button(root, text='Enflasyon Hesapla',
-              command=(lambda e=ents: enflasyon_hesap(e)))
-   b1.pack(side=LEFT, padx=5, pady=5)
-   root.mainloop()
-   
