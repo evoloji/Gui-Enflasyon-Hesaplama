e116786d237a0c66fd91646329cb2e041ac00b14
evoloji <evoloji@192.168.1.14>
2024-10-14 13:38:47 +0300

    iyileştirmeler

--- ehesap.py
+++ ehesap.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 
 
@@ -18,9 +19,9 @@ def enflasyon_hesap(entries):
 
 
     #Tarihli entryleri tanımlama
-    police_ilk = str(entries["Poliçe Başlangıç Tarihi"].get())
+    police_baslangic_tarihi = str(entries["Poliçe Başlangıç Tarihi"].get())
     hasar_tar =  str(entries["Hasar Tarihi"].get())
-    pit = datetime.datetime.strptime(police_ilk, "%d.%m.%Y")
+    pit = datetime.datetime.strptime(police_baslangic_tarihi, "%d.%m.%Y")
     htt = datetime.datetime.strptime(hasar_tar, "%d.%m.%Y")
 
     try:
@@ -40,6 +41,12 @@ def enflasyon_hesap(entries):
     enflasyon_artis_katsayi = ((float(gunsayi) /vade) * enf_oran) + 1
     enf_sig_bed = enflasyon_artis_katsayi *sig_bed
 
+    # Sonuçları güncelleme
+    output_vars["Katsayı"].set(f"{enflasyon_artis_katsayi:.7f}")  # Katsayıyı güncelle
+    enf_sig_bed_str = locale.format_string("%0.2f", enf_sig_bed, grouping=True) + " TL"
+    output_vars["Enflasyonlu Sigorta Bedeli"].set(enf_sig_bed_str)  # Sigorta bedelini güncelle
+
+    
     #Sigorta Bedeli * Katsayı
     enflasyon_artis_katsayi = ("%0.7f" % enflasyon_artis_katsayi).strip()
 
@@ -59,7 +66,7 @@ def enflasyon_hesap(entries):
     if 'left' in globals():
      left.destroy()  # Mevcut label'ı temizle
     
-    labelframe = LabelFrame(root,text = "Hasarlı Gün Sayısı")
+    labelframe = LabelFrame(root,text = "Başlangıçtan İtibaren Geçen Gün Sayısı")
     labelframe.pack(fill = "both", expand = "yes")
     
     left = Label(labelframe, text = sayi)
@@ -68,6 +75,7 @@ def enflasyon_hesap(entries):
 #Şablon oluşturması    
 def makeform(root, alanlar):
    entries = {}
+   output_vars = {}
    for field in alanlar:
       row = Frame(root)
       lab = Label(row, width=30, text=field+": ", anchor='w')
@@ -77,15 +85,18 @@ def makeform(root, alanlar):
       lab.pack(side=LEFT)
       ent.pack(side=RIGHT, expand=YES, fill=X)
       entries[field] = ent
-   return entries
+      # Eğer çıkış alanı ise StringVar oluştur
+      if field in ["Enflasyonlu Sigorta Bedeli", "Katsayı"]:
+        var = StringVar()
+        ent.config(state='readonly')  # Okunmaz hale getir
+        ent['textvariable'] = var
+        output_vars[field] = var
+   return entries, output_vars
 
 if __name__ == '__main__':
    root = Tk()
-   ents = makeform(root, alanlar)
-   root.title("Enflasyon Hesap")
-
-   b1 = Button(root, text='Enflasyon Hesapla', 
-               command=(lambda e=ents: enflasyon_hesap(e)))
+   ents, output_vars = makeform(root, alanlar)
+   root.title("Hasar Tarihli Enflasyon Hesaplama")
+   b1 = Button(root, text='Enflasyon Hesapla',  command=(lambda e=ents: enflasyon_hesap(e)))
    b1.pack(side=LEFT, padx=5, pady=5)
-
    root.mainloop()
